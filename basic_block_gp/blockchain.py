05c07275e6db1c926cf25aba9f1bb1f53c29de7a
Nick Gonzalez <gonzaleznick5@gmail.com>
2020-02-12 13:20:19 -0500

    Blockchain_Starter

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -12,6 +12,7 @@ class Blockchain(object):
         self.current_transactions = []
 
         # Create the genesis block
+        # One is here sine its not created like the rest of the hashes
         self.new_block(previous_hash=1, proof=100)
 
     def new_block(self, proof, previous_hash=None):
@@ -31,13 +32,19 @@ class Blockchain(object):
         """
 
         block = {
-            # TODO
+           'index': len(self.chain) + 1,
+           'timestamp': time(),
+           'transaction': self.current_transactions,
+           'proof': proof,
+           'previous_hash': previous_hash or self.hash(self.chain[-1]),
         }
 
         # Reset the current list of transactions
-        # Append the chain to the block
+        self.current_transactions = []
+        # Append the block to the chain
+        self.chain.append(block)
         # Return the new block
-        pass
+        return block
 
     def hash(self, block):
         """
@@ -119,6 +126,7 @@ def mine():
 
     response = {
         # TODO: Send a JSON response with the new block
+        "message": "yo world"
     }
 
     return jsonify(response), 200
